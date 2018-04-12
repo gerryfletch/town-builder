018096ebaa3dfad833d1ac6b821b69276cdb3982
gerryfletch <1997gerry@gmail.com>
2018-04-13 00:06:14 +0100

    Created Web IDE html/css structure with some nav items

---
+++ src/app/components/web-ide/web-ide.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'web-ide',
+  templateUrl: './web-ide.component.html',
+  styleUrls: ['./web-ide.component.scss']
+})
+export class WebIdeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
