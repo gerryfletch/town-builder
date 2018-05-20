d92675d8066d500782024606bfc8749f2d9fa602
gerryfletch <1997gerry@gmail.com>
2018-05-20 22:43:47 +0100

    Added in About section [commit fix]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,17 +11,17 @@ import {HttpModule} from '@angular/http';
 import {WebIdeComponent} from './components/web-ide/web-ide.component';
 import {WebIdeService} from './_services/web-ide.service';
 import {AceEditorModule} from 'ng2-ace-editor';
-import { SafeHtmlPipe } from './_pipes/safe-html.pipe';
+import {SafeHtmlPipe} from './_pipes/safe-html.pipe';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { PropsService } from './_services/props.service';
-import { MapComponent } from './components/game/map/map.component';
-import { HeaderComponent } from './components/game/header/header.component';
+import {PropsService} from './_services/props.service';
+import {MapComponent} from './components/game/map/map.component';
+import {HeaderComponent} from './components/game/header/header.component';
 import {AboutComponent} from './components/about/about/about.component';
-import { RoutingModule } from './routing.module';
-import { BuilderComponent } from './components/builder/builder.component';
-import { EditorComponent } from './components/builder/editor/editor.component';
-import { ResourcesComponent } from './components/builder/resources/resources.component';
-import { InventoryComponent } from './components/builder/inventory/inventory.component';
+import {RoutingModule} from './routing.module';
+import {BuilderComponent} from './components/builder/builder.component';
+import {EditorComponent} from './components/builder/editor/editor.component';
+import {ResourcesComponent} from './components/builder/resources/resources.component';
+import {InventoryComponent} from './components/builder/inventory/inventory.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +37,7 @@ import { InventoryComponent } from './components/builder/inventory/inventory.com
     BuilderComponent,
     EditorComponent,
     ResourcesComponent,
-    InventoryComponent
+    InventoryComponent,
     AboutComponent
   ],
   imports: [
